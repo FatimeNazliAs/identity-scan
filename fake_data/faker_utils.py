147ae00d5011ef88431051b135b3d1f10f36d062
Nazlı As <f.nazlias@gmail.com>
2025-05-05 16:01:28 +0300

    add utility to generate fake personal information for synthetic data
    - Uses the Faker library to generate a fake name, surname, 11-digit ID number, and birth date.

---
+++ fake_data/faker_utils.py
@@ -0,0 +1,13 @@
+from faker import Faker
+
+
+def create_fake_info():
+    fake = Faker()
+    fake_info_dict = {
+        "id_number": fake.numerify("###########"),
+        "surname": fake.name().split()[1],
+        "name": fake.name().split()[0],
+        "birth_date": str(fake.date_of_birth()).replace("-", "."),
+    }
+
+    return fake_info_dict
