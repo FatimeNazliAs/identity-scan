0de89ecce44e3ba99c131dc3b72f5f895ce8ed49
Nazlı As <f.nazlias@gmail.com>
2025-05-20 14:20:18 +0300

    generate 100 fake samples for YoLo training

--- fake_data_generation/generate_sample.py
+++ fake_data_generation/generate_sample.py
@@ -23,14 +23,14 @@ from image_utils import save, initialize_template
 import os
 
 if __name__ == "__main__":
-    base_path = "fake_data_creation"
+    base_path = "fake_data_generation"
     img_folder = os.path.join(base_path, "synthetic_data", "images")
     txt_folder = os.path.join(base_path, "synthetic_data", "labels")
 
     os.makedirs(img_folder, exist_ok=True)
     os.makedirs(txt_folder, exist_ok=True)
 
-    for index in range(11, 31):
+    for index in range(101, 102):
         template_image, draw, fill, font, align = initialize_template()
         txt_file_path = os.path.join(txt_folder, f"{index}.txt").replace("\\", "/")
         img_file_path = os.path.join(img_folder, f"{index}.png").replace("\\", "/")
