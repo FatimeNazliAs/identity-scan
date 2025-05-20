4649214002a30bc853f16501a43d6748d0ea6291
Nazlı As <f.nazlias@gmail.com>
2025-05-20 14:17:39 +0300

    change datetime to date in birth_date and created_at

--- app/models.py
+++ app/models.py
@@ -1,6 +1,5 @@
-from datetime import datetime
+from datetime import date
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
 from .database import Base
 
 
@@ -9,10 +8,8 @@ class IdentityCard(Base):
     __tablename__ = "turkish_identity_cards"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    identity_number: Mapped[str] = mapped_column(
-        String(11), unique=True, nullable=False
-    )
+    identity_number: Mapped[str] = mapped_column(unique=True, nullable=False)
     surname: Mapped[str] = mapped_column(nullable=False)
     name: Mapped[str] = mapped_column(nullable=False)
-    birth_date: Mapped[datetime] = mapped_column(nullable=False)
-    created_at: Mapped[datetime] = mapped_column(nullable=False)
+    birth_date: Mapped[date] = mapped_column(nullable=False)
+    created_at: Mapped[date] = mapped_column(nullable=False)
