4d47381ea2904907065ba529d1bcbacce5066221
Nazlı As <f.nazlias@gmail.com>
2025-05-20 16:12:29 +0300

    add FastAPI endpoints for file upload, inference, and database operations
    - Create API routes for handling identity card image uploads, inference processing, and storing results in the database.
    
    - Add POST endpoint `/identity_cards/save_file/` to upload image files, clear previous PNGs, and store new ones.
    
    - Implement GET endpoint `/identity_cards/show_inference_results/` to run inference, create or clear JSON files, and save results.
    
    - Add POST endpoint `/identity_cards/save_inference_results/` to convert JSON results into a SQLAlchemy IdentityCard object and commit to the database.
    
    - Create GET endpoint `/identity_cards/get_inference_results/` to retrieve all stored identity records from the database.
    
    - Integrate CRUD utilities and database session dependency using `Depends(get_db)` and SQLAlchemy ORM.

---
+++ app/main.py
@@ -0,0 +1,66 @@
+from fastapi import FastAPI, UploadFile, Depends
+from .database import engine, get_db
+from sqlalchemy.orm import Session
+from .models import Base, IdentityCard
+from .crud import (
+    save_uploaded_file,
+    create_upload_directory,
+    save_inference_result_to_json,
+    save_identity_card_from_json,
+    show_inference_result,
+    delete_existing_png_files,
+    create_json_file,
+    get_latest_path,
+    delete_content_in_json
+    
+)
+from typing import Annotated, List
+from .schemas import IdentityCardRequest, IdentityCardResponse
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@app.post("/identity_cards/save_file/")
+async def save_file(file: UploadFile):
+    file_dir = create_upload_directory()
+    delete_existing_png_files(file_dir)
+    file_full_path = save_uploaded_file(file, file_dir)
+    return {"message": file_full_path}
+
+
+@app.get("/identity_cards/show_inference_results/", response_model=IdentityCardRequest)
+async def show_inference_results():
+
+    save_dir = "app/upload_file"
+
+    result = show_inference_result()
+    json_file_path = get_latest_path(file_type="json")
+    if not json_file_path:        
+        json_file_path = create_json_file(save_dir)
+    else:
+        delete_content_in_json(json_file_path)
+    save_inference_result_to_json(result, json_file_path)
+    return result
+
+
+@app.post(
+    "/identity_cards/save_inference_results/", response_model=IdentityCardResponse
+)
+async def save_inference_results(db: db_dependency):
+    identity_card = save_identity_card_from_json()
+    db.add(identity_card)
+    db.commit()
+    db.refresh(identity_card)
+
+    return identity_card
+
+
+@app.get(
+    "/identity_cards/get_inference_results", response_model=List[IdentityCardResponse]
+)
+async def get_inference_results(db: db_dependency):
+    results = db.query(IdentityCard).all()
+    return results
